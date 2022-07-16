308aac485955e5a0f9dd8a11b82cc85bf81c061d
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-07-16 16:22:40 -0500

    final mvp

--- api/server.js
+++ api/server.js
@@ -5,8 +5,31 @@ const User = require('./users/model');
 const server = express();
 server.use(express.json());
 
-server.put('/api/users/:id', (req, res) => {
-
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const editingUser = await User.findById(req.params.id)
+        if (!editingUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user"
+                })
+            } else {
+                const updatedUser = await User.update(
+                    req.params.id, 
+                    req.body
+                    )
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({ 
+            message: "The user information could not be modified" 
+        })
+    }
 })
 
 server.delete('/api/users/:id', async (req, res) => {
