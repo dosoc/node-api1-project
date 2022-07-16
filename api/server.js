8fb85cacf16b7ae57158543b3085e59cd3c153c5
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-07-16 15:31:41 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,25 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
-
+const User = require('./users/model')
 const server = express();
+
+server.get('/api/user/:id', (req, res) => {
+    
+})
+
+server.get('/api/users', (req, res)=> {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack: err.stack
+            })
+        })
+})
 
 server.use('*', (req, res) => {
     res.status(404).json({
@@ -9,8 +27,6 @@ server.use('*', (req, res) => {
     })
 })
 
-server.get('/', (req, res)=> {
-    res.send('Hello, World!');
-})
+
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
